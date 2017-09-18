49489092b2d833debbf7034a838da1d753af48b2
saurabh2799 <31250615+saurabh2799@users.noreply.github.com>
2017-09-19 00:17:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+    res.send(www.google.co.in);
+    
+ // res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
